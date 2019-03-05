3fad474335adf9657e6643419c0761b7770c284e
robson <nosbor84@gmail.com>
2019-03-05 19:48:57 -0300

    Global Reg. components and BaseIcon component

---
+++ src/components/BaseButton.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <button>
+      <slot>Submit</slot>
+    </button>
+  </div>
+</template>
--- src/views/EventCard.vue
+++ src/views/EventCard.vue
@@ -7,7 +7,7 @@
       <div class="event-card -shadow">
         <span class="eyebrow">@{{ event.time }} on {{ event.data }}</span>
         <h4 class="title">{{ event.title }}</h4>
-        <span>{{ event.attendees.length }} attending</span>
+        <BaseIcon name="users">{{ event.attendees.length }} attending</BaseIcon>
       </div>
     </router-link>
   </div>
